3c565ac9befbc3742e3096ccd519d43722dad3c5
Andrea Palomo <Andrea Palomo@LAPTOP-CAFNLIF9>
2018-02-24 19:41:33 -0600

    estoy trabajando en mi tarea commit

--- src/ipc1/tarea3_201700670/IPC1Tarea3_201700670.java
+++ src/ipc1/tarea3_201700670/IPC1Tarea3_201700670.java
@@ -28,19 +28,28 @@ public class IPC1Tarea3_201700670 {
         System.out.println("3. TRES NÚMEROS DE MAYOR A MENOR");
         System.out.println("4. CALCULAR PROMEDIO");
         System.out.println("5. SALIR");
+        System.out.print("¿Qué opción desea elegir (1-5)? ");
         res=entrada.nextInt(); //se ingresa el número de la opción del menú
-        
+        System.out.println("");
         switch(res){
             case 1://es cada opción a realizar y su debido procedimiento de realización
-                
+                Usuarios llama= new Usuarios();
+                llama.Usuarios(args);//llama a todos los elementos de los usuarios
                 break;
             case 2:
+                Contadigi llamado= new Contadigi();
+                llamado.Contadigi(args);//Llama a todos los elementos de contador de dígitos
                 break;
             case 3:
+                Mayorme entra= new Mayorme();
+                entra.Mayorme(args);//Llama a todos los elementos de mayor y menor de tres números
                 break;
             case 4:
+                Promedio entrado= new Promedio();
+                entrado.Promedio(args);//Llama a todos los elementos de un Promedio
                 break;
             case 5:
+                System.out.println("GRACIAS POR UTILIZAR EL PROGRAMA");
                 System.exit(0); //salir del programa
                 break;
             default:
